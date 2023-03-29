ac1f9a852c05218a3cec8d5aecbf4f6293b203ef
Samarendra <samarendrapani1998@gmail.com>
2023-03-29 15:43:51 +0200

    added --output option and total alignment length

--- snakemake_pipelines/annotating-paths/scripts/alignment_stats.py
+++ snakemake_pipelines/annotating-paths/scripts/alignment_stats.py
@@ -1,8 +1,7 @@
 import argparse
-import sys
 import logging
 import resource
-from collections import defaultdict, namedtuple, Counter
+from collections import defaultdict
 import gzip
 import matplotlib.pyplot as plt
 
@@ -22,6 +21,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true", default=False, help="Print debug messages")
     parser.add_argument("--assembly-list", required=True, help="Text file with the list of assembly-to-graph GAF files. The GAF files should be sorted.")
+    parser.add_argument("--output", required=True, help="Output directory to store all the stats and graphs.")
     
     options = parser.parse_args()
     setup_logging(options.debug)
@@ -34,21 +34,22 @@ def main():
             gaf_files.append(line.rstrip())
     
     for gaf in gaf_files:
-        get_stats(gaf)
+        get_stats(gaf, options.output)
 
     memory_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     logger.info("\nMemory Information")
     logger.info("  Maximum memory usage: %.3f GB", memory_kb / 1e6)
     
 
-def get_stats(gaf):
-    logger.info("\nINFO: Working on file %s"%(gaf))
+def get_stats(gaf, out):
+    print("\nINFO: Working on file %s"%(gaf))
     if is_file_gzipped(gaf):
         reader = gzip.open(gaf, 'rt')
     else:
         reader = open(gaf, 'r')
     stats = defaultdict(lambda: [])
     total_alignments = 0
+    total_alignment_len = 0
     num25 = 0
     num50 = 0
     num75 = 0
@@ -63,6 +64,7 @@ def get_stats(gaf):
         seq_name = alignment[0]
         q_len = int(alignment[1])
         q_start = int(alignment[2])
+        total_alignment_len += abs(q_start-q_end)
         q_end = int(alignment[3])
         percentage_aligned = abs(q_start-q_end)/q_len
         num_res_match = int(alignment[9])
@@ -112,38 +114,36 @@ def get_stats(gaf):
         per_read_stat['dv'].append(stat[index][4]['dv'])
 
     
-    logger.info("\tFound %d alignments (where %d are primary alignments) from %d sequences."%(total_alignments, num_prim, len(list(stats.keys()))))
-    logger.info("\tAlignment with percentage query length between:")
-    logger.info("\t\t0.00 and 0.25: %d"%(num25))
-    logger.info("\t\t0.25 and 0.50: %d"%(num50))
-    logger.info("\t\t0.50 and 0.75: %d"%(num75))
-    logger.info("\t\t0.75 and 0.90: %d"%(num90))
-    logger.info("\t\t0.90 and 1.00: %d"%(num100))
+    print("\tFound %d alignments (where %d are primary alignments) from %d sequences."%(total_alignments, num_prim, len(list(stats.keys()))))
+    print("\tAlignment with percentage query length between:")
+    print("\t\t0.00 and 0.25: %d"%(num25))
+    print("\t\t0.25 and 0.50: %d"%(num50))
+    print("\t\t0.50 and 0.75: %d"%(num75))
+    print("\t\t0.75 and 0.90: %d"%(num90))
+    print("\t\t0.90 and 1.00: %d"%(num100))
+    print("\tTotal base pairs in alignment (not corrected): %d"%(total_alignment_len))
 
-    graph(per_read_stat, total_stat, num_align)
+    graph(per_read_stat, total_stat, num_align, gaf, out)
 
-def graph(read, total, align):
+def graph(read, total, align, gaf, out):
     
+    hap = "_".join(gaf.split("/")[-1].split(".")[0:2])
     for y_label in read.keys():
         read_data = read[y_label]
         total_data = total[y_label]
-
+        p = hap+"_"+y_label
         fig, axes = plt.subplots(2, 1, figsize=(15, 15))
         plt.suptitle(y_label)
         axes[0].hist(read_data, bins=20, rwidth = 0.8)
         axes[1].hist(total_data, bins=20, rwidth = 0.8)
         axes[0].set_title('Per Read Data (Selected Alignment with Highest Alignment Length)')
         axes[1].set_title('Data for all the alignments')
-        plt.savefig('/home/samarendra/Pictures/'+y_label+'.png')
-        #plt.show()
+        plt.savefig(out+p+'.png')
 
     fig, axes = plt.subplots(1, 1, figsize=(15, 10))
     axes.hist(align, bins=20, rwidth = 0.8)
     axes.set_title('Number of Alignments per read')
-    plt.savefig('/home/samarendra/Pictures/num_align.png')
-    #plt.show()
-
-    pass
+    plt.savefig('%s%s_num_align.png'%(out,hap))
 
 
 def is_file_gzipped(src):
