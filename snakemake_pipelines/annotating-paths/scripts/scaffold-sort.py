154e205c886ca05be4c38e4cc61fb4e82e2dd5d6
Samarendra <samarendrapani1998@gmail.com>
2023-06-13 09:24:54 +0200

    updated scaffold sort inputs

--- snakemake_pipelines/annotating-paths/scripts/scaffold-sort.py
+++ snakemake_pipelines/annotating-paths/scripts/scaffold-sort.py
@@ -6,6 +6,15 @@ Using this definition of scaffold nodes, the sorting has been done.
 
 The main difference between this sorting and the bubble-sort.py script is that here the the entire alignment is processed and used to determine whether the alignment is reveresed.
 Here inversions are not taken into account.
+
+Index file created:
+The index is dictionary created using pickle library which contains the reference contig names as keys and the offset the alignments begin and end as a list of size 2.
+
+Other things the code does:
+It adds some tags into the sorted gaf file. The tags are:
+    1. bo:i: - This is the BO tags from the GFA carried forward. The sorting uses a BO tag for each alignment, the BO tag for the start node (or end node based on overall orientation) of the alignment path.
+    2. sn:Z: - The name of the reference contig it mapped to.
+    3. iv:i: - 1 if the alignment path has an inversion. 0 otherwise.
 '''
 
 import sys
@@ -17,6 +26,7 @@ import gzip
 import re
 from pysam import libcbgzf
 import resource
+import pickle as pkl
 from whatshap.timer import StageTimer
 
 logger = logging.getLogger(__name__)
@@ -33,7 +43,8 @@ def main():
     parser.add_argument("--debug", action="store_true", default=False, help="Print debug messages")
     parser.add_argument("--gfa", required=True, help="GFA file with the sort keys (BO and NO tagged)")
     parser.add_argument("--gaf", required=True, help="Input GAF File")
-    parser.add_argument("--output", default=None, help="Output GAF File path (Default: sys.stdout)")
+    parser.add_argument("--outgaf", default=None, help="Output GAF File path (Default: sys.stdout)")
+    parser.add_argument("--outind", default=None, help="Output Index File path for the GAF file. (When --outgaf is not given, no index is created. If it is given and --outind is not specified, it will have same file name with .gai extension)")
     parser.add_argument("--bgzip", action='store_true', help="Flag to bgzip the output. Can only be given with --output.")
     
     options = parser.parse_args()
@@ -56,47 +67,56 @@ def main():
 
 def bubble_sort(options):
     
-    if options.output == None:
+    if options.outgaf == None:
         writer = sys.stdout
+        index_file = None
     else:
         if options.bgzip:
-            writer = libcbgzf.BGZFile(options.output, 'wb')
+            writer = libcbgzf.BGZFile(options.outgaf, 'wb')
         else:
-            writer = open(options.output, "w")
-    nodes = defaultdict(lambda: [-1,-1])
+            writer = open(options.outgaf, "w")
+        if options.outind:
+            index_file = options.outind
+        else:
+            index_file = options.outgaf+".gai"
+    nodes = defaultdict(lambda: [-1,-1,-1,-1])
+    index_dict = defaultdict(lambda: [None, None])
     with timers("read_gfa"):
         read_gfa(options.gfa, nodes)
     with timers("total_sort"):
-        sort(options.gaf, nodes, writer)
+        sort(options.gaf, nodes, writer, index_dict)
     writer.close()
+    index_dict = dict(index_dict)
+    with open(index_file, "wb") as ind:
+        pkl.dump(index_dict, ind)
+    
     
 
-def sort(gaf, nodes, writer):
+def sort(gaf, nodes, writer, index_dict):
     
-    logger.info("\n##### Parsing GAF file and sorting it #####")
+    logger.info("Parsing GAF file and sorting it")
     if is_file_gzipped(gaf):
         reader = gzip.open(gaf, 'rt')
     else:
         reader = open(gaf, 'r')
     
-    Alignment = namedtuple('Alignment', ['offset', 'BO', 'NO', 'start', 'confused'])
+    Alignment = namedtuple('Alignment', ['offset', 'BO', 'NO', 'start', 'inv', 'sn'])
     gaf_alignments = []
     count_inverse = 0
     # First pass: Store all the alignment lines as minimally. Just storing line offset and alignment string.
     with timers("read_gaf"):
-        logger.debug("Processing and Storing GAF Alignments...")
         while True:
             offset = reader.tell()
             line = reader.readline()
             if not line:
                 break
             line = line.split('\t')
-            bo, no, start, confused = process_alignment(line, nodes, offset)
-            if confused:
+            bo, no, start, inv, sn = process_alignment(line, nodes, offset)
+            if inv == 1:
                 count_inverse += 1
-            gaf_alignments.append(Alignment(offset=offset, BO=bo, NO=no, start=start, confused=confused))
+            gaf_alignments.append(Alignment(offset=offset, BO=bo, NO=no, start=start, inv=inv, sn=sn))
     
-    logger.info("Number of alignments with scaffold nodes in both orientation: %d"%(count_inverse))
+    logger.info("\tNumber of alignments with invertions: %d"%(count_inverse))
     # Sorting the alignments based on BO and NO tag
     with timers("sort_gaf"):
         logger.debug("Sorting the alignments...")
@@ -107,17 +127,22 @@ def sort(gaf, nodes, writer):
         logger.debug("Writing Output File...")
         for alignment in gaf_alignments:
             off = alignment.offset
-            if alignment.confused:
-                logger.debug("[DEBUG|INV] Offset of read in sorted file: %d"%(writer.tell()))
             reader.seek(off)
-            line = reader.readline()
+            line = reader.readline().rstrip()
+            line += "\tbo:i:%d\tsn:Z:%s\tiv:i:%d\n"%(alignment.BO, alignment.sn, alignment.inv)
+            out_off = writer.tell()
+            if index_dict[alignment.sn][0] == None:
+                index_dict[alignment.sn][0] = out_off
+                index_dict[alignment.sn][1] = out_off
+            else:
+                index_dict[alignment.sn][1] = out_off
             write_to_file(line, writer)
-    
+    index_dict.pop('unknown')
     reader.close()
 
 def read_gfa(gfa, node):
     
-    logger.info("\n##### Parsing GFA file and reading sort key information #####")
+    logger.info("Parsing GFA file and reading sort key information")
     if is_file_gzipped(gfa):
         reader = gzip.open(gfa, 'rt')
     else:
@@ -139,9 +164,13 @@ def read_gfa(gfa, node):
                 tagged_nodes += 1
             if f.startswith("NO:i:"):
                 node[fields[1]][1] = int(f[5:])
+            if f.startswith("SR:i:"):
+                node[fields[1]][2] = int(f[5:])
+            if f.startswith("SN:Z:"):
+                node[fields[1]][3] = f[5:]
             
-    logger.info("Total Nodes Processed: %d"%(total_nodes))
-    logger.info("Nodes with tags: %d"%(tagged_nodes))
+    logger.info("\tTotal Nodes Processed: %d"%(total_nodes))
+    logger.info("\tNodes with tags: %d"%(tagged_nodes))
     reader.close()
 
 
@@ -155,19 +184,30 @@ def process_alignment(line, nodes, offset):
     no = None
     start = None
     orient_list = []
-    confused = False
+    inv = 0     # Whether the alignment has an inversion
+    sn = None
     for n in path:
         if n in [">", "<"]:
             orient = n
             continue
+        
+        # Finding the chromosome where the alignment is
+        if sn == None and nodes[n][2] == 0:
+            sn = nodes[n][3]
+        elif nodes[n][2] == 0:
+            assert sn == nodes[n][3]
+        
         if nodes[n][0] == -1 or nodes[n][1] == -1:
             logger.debug("[ERR]\tOF:i:%d\tND:Z:%s"%(offset,n))
             continue
+        # Skipping the non-scaffold nodes
         if nodes[n][1] != 0:
             continue
+        # Only keeping the orientation of the scaffold nodes in the path matching
         orient_list.append(orient)
+    # If there are scaffold nodes in both direction, this indicates an inversion
     if orient_list.count('>') !=0 and orient_list.count('<') != 0:
-        confused = True
+        inv = 1
     # TODO: What to do when the start node that we have is an untagged node?
     # One argument is to just sort them to the end and completely ignore them since they dont belong to any vcf bubble.
     # That works for my tools but not ideal for general purpose.
@@ -183,7 +223,9 @@ def process_alignment(line, nodes, offset):
         n = path[1]
         bo = nodes[n][0]
         no = nodes[n][1]
-    return bo, no, start, confused
+    if sn == None:
+        sn = "unknown"
+    return bo, no, start, inv, sn
 
 
 def compare_gaf(al1, al2):
@@ -237,8 +279,10 @@ def is_file_gzipped(src):
 
 
 def validate_arguments(options):
-    if options.bgzip and not options.output:
+    if options.bgzip and not options.outgaf:
         raise RuntimeError("--bgzip flag has been specified but not output path has been defined. Please define the output path.")
+    if options.outind and not options.outgaf:
+        raise RuntimeError("index path specified but no output gaf path. Please provide an output path.")
 
 
 if __name__ == "__main__":
