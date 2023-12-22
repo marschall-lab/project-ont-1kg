10e9162d4551ea6fb15a32ecca1916e827a322b4
Samarendra <samarendrapani1998@gmail.com>
2023-12-22 13:13:58 +0100

    dividing script into two files

---
+++ snakemake_pipelines/augmented_graph/scripts/create-partial-output-sv_count.sh
@@ -0,0 +1,18 @@
+# This script is to create output files when the snakemake pipeline has not completely run.
+# The output is created in the subdirectory ../temp/
+
+#!/bin/bash
+
+# creating list of vcfs
+ls /gpfs/project/projects/medbioinf/users/spani/results/1000GP/augmented_graph/minigraph-extended_all/genotypes/*-biallelic.vcf.gz > ../temp/biallelic-vcfs.txt
+
+# activating conda environment used in snakemake pipeline
+conda activate /gpfs/project/projects/medbioinf/users/spani/scripts/1000g-ont/ont-1kg/snakemake_pipelines/augmented_graph/.snakemake/conda/91103c7d858528647eb601a6ea8dffb7_
+
+# creating comma separate list of biallelic vcfs
+csl_biallelic=`awk '{print $1}' ../temp/biallelic-vcfs.txt | paste -s -d, -`
+
+# creating SV count plots
+sv_count_script="/gpfs/project/projects/medbioinf/users/spani/scripts/1000g-ont/ont-1kg/snakemake_pipelines/augmented_graph/scripts/sv_count.py"
+metadata="/gpfs/project/projects/medbioinf/users/spani/files/other/1000GP/igsr_sample_data.tsv"
+python $sv_count_script -meta $metadata -vcf $csl_biallelic -output ../temp/ 2> ../temp/sv_count.log
