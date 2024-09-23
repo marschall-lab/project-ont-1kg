0cc3f41fd11c2d8630c443c383558f6f33295916
Samarendra <samarendrapani1998@gmail.com>
2024-09-23 10:52:28 +0200

    format change. adding all svs to plot

--- snakemake_pipelines/chimpanzee-analysis/scripts/plot-sv-lengths.py
+++ snakemake_pipelines/chimpanzee-analysis/scripts/plot-sv-lengths.py
@@ -6,6 +6,7 @@ import numpy as np
 def run(vcf=None, outdir=None):
     
     sv_lens = []
+    all_sv_lens = []
     # reading the callset vcf
     callset_reader = open(vcf, 'r')
     for line in callset_reader:
@@ -14,6 +15,7 @@ def run(vcf=None, outdir=None):
         sv_id=line.split('\t')[2]
         aa_field=line.split('\t')[7].split(';')[-1]
         assert aa_field.startswith('ANCESTRAL_ALLELE')
+        all_sv_lens.append(int(sv_id.split('-')[-1]))
         if aa_field.split('=')[-1] == '.':
             continue
         sv_lens.append(int(sv_id.split('-')[-1]))
@@ -21,9 +23,15 @@ def run(vcf=None, outdir=None):
     
     # Plotting length histograms
     fig, ax = plt.subplots(1, 1, figsize=(10,10), tight_layout=True)
-    ax.hist(sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='blue')
-    ax.set_title('Lengths of SVs annotated with an ancestral allele')
+    ax.hist(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='blue', label='All SVs')
+    ax.hist(sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='orange', label='SVs with known Ancestral All')
+    ax.set_title('Lengths of SVs annotated with an Ancestral Allele', fontsize=16)
+    plt.xlabel('Lengths', fontsize=16)
+    plt.ylabel('Counts', fontsize=16)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
     plt.yscale('log')
+    plt.legend(prop={'size': 16})
     plt.savefig(outdir+'sv-length-dist.svg')
 
 if __name__=='__main__':
