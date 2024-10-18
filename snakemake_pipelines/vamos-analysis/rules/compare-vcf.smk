9c38002271bd0eaf344b17a531fcb1f6cc90f3ba
Samarendra <samarendrapani1998@gmail.com>
2024-10-18 18:14:13 +0200

    adding rules for callset comparision between miller vntrs and svan vntrs

---
+++ snakemake_pipelines/vamos-analysis/rules/compare-vcf.smk
@@ -0,0 +1,94 @@
+# making the vcf compatible for the comparison pipeline
+rule prepare_miller_vcf:
+    input:
+        vcf='results/analysis/vamos-miller.sorted.vcf',
+        ref='results/reference-vntrs.bed'
+    output:
+        'results/callset-comparison/miller-vcfs/{sample}.vcf'
+    shell:
+        'python scripts/subsample-vcf.py -vcf {input.vcf} -reference {input.ref} -sample {wildcards.sample} > {output}'
+
+# adding SVLEN and SVTYPE tags to the miller vcf
+rule add_tags_miller:
+    input:
+        'results/callset-comparison/miller-vcfs/{sample}.vcf'
+    output:
+        'results/callset-comparison/miller-vcfs/{sample}.tagged.vcf'
+    log:
+        'results/callset-comparison/miller-vcfs/{sample}.tagged.log'
+    conda:
+        "../envs/comparison.yml"
+    shell:
+        "cat {input} | python scripts/add-svtags.py 2> {log} 1> {output}"
+
+# extracting the VNTR sites from SVAN annotations and adding genotypes to them
+rule prepare_svan_annot:
+    input:
+        insertions=config['path_to_svan_ins'],
+        deletions=config['path_to_svan_del'],
+        gts=config['path_to_gts']
+    output:
+        ins_gts=temp('results/callset-comparison/svan-vcfs/ins-gts.vcf'),
+        del_gts=temp('results/callset-comparison/svan-vcfs/del-gts.vcf'),
+        vntr_exp=temp('results/callset-comparison/svan-vcfs/vntrs-exp.vcf'),
+        vntr_con=temp('results/callset-comparison/svan-vcfs/vntrs-con.vcf'),
+        vntrs='results/callset-comparison/svan-vcfs/vntrs.vcf'
+    conda:
+        '../envs/comparison.yml'
+    shell:
+        '''
+        bcftools annotate -c INFO -a {input.insertions} {input.gts} > {output.ins_gts}
+        bcftools annotate -c INFO -a {input.deletions} {input.gts} > {output.del_gts}
+        bcftools view -i "ITYPE_N=\'VNTR\'" {output.ins_gts} | bcftools sort > {output.vntr_exp}
+        bcftools view -i "DTYPE_N=\'VNTR\'" {output.del_gts} | bcftools sort > {output.vntr_con}
+        bcftools concat {output.vntr_exp} {output.vntr_con} > {output.vntrs}
+        '''
+
+# extract samples from the SVAN vntrs
+rule extract_subsample_svan:
+    input:
+        'results/callset-comparison/svan-vcfs/vntrs.vcf'
+    output:
+        'results/callset-comparison/svan-vcfs/{sample}.vcf'
+    conda:
+        '../envs/comparison.yml'
+    shell:
+        'bcftools view --samples {wildcards.sample} {input} > {output}'
+
+# adding SVLEN and SVTYPE tags to the SVAN vcf
+rule add_tags_svan:
+    input:
+        'results/callset-comparison/svan-vcfs/{sample}.vcf'
+    output:
+        'results/callset-comparison/svan-vcfs/{sample}.tagged.vcf'
+    log:
+        'results/callset-comparison/svan-vcfs/{sample}.tagged.log'
+    conda:
+        '../envs/comparison.yml'
+    shell:
+        'cat {input} | python scripts/add-svtags.py 2> {log} 1> {output}'
+
+# intersecting the vcfs
+# the order of the vcfs given to the intersection matters
+rule intersect_vcfs:
+    input:
+        'results/callset-comparison/svan-vcfs/{sample}.tagged.vcf',
+        'results/callset-comparison/miller-vcfs/{sample}.tagged.vcf'
+    output:
+        tsv="results/callset-comparison/intersection/{sample}.tsv",
+        vcf="results/callset-comparison/intersection/{sample}.vcf",
+        pdf="results/callset-comparison/intersection/{sample}.pdf",
+        plot="results/callset-comparison/intersection/upsetplot_{sample}.pdf"
+    conda:
+        "../envs/comparison.yml"
+    log:
+        intersect="results/callset-comparison/intersection/intersect-{sample}.log",
+        plot="results/callset-comparison/intersection/plot-{sample}.log"
+    params:
+        names = ['svan', 'miller'],
+        columns = ['in_svan', 'in_miller']
+    shell:
+        '''
+        python scripts/intersect_callsets.py intersect -c {input} -n {params.names} -t {output.tsv} -v {output.vcf} -p {output.pdf} --id-from-vcf &> {log.intersect}
+        python scripts/plot-upset.py -t {output.tsv} -o {output.plot} -n {params.columns} &> {log.plot}
+        '''
---
+++ snakemake_pipelines/vamos-analysis/scripts/subsample-vcf.py
@@ -0,0 +1,106 @@
+# Judge me not based on this script. I am overcaffeinated and sleepless now.
+
+import argparse
+
+def convert_to_seq(anno, rus):
+    anno = anno.split('-')
+    seq = ''
+    for a in anno:
+        seq += rus[int(a)]
+    return seq
+
+def run(vcf=None, sample=None, reference=None):
+
+    reference_dict = {}
+    reader = open(reference, 'r')
+    for line in reader:
+        chrom, start, end, seq = line.strip().split('\t')
+        reference_dict[(chrom, start, end)] = seq
+    reader.close()
+    
+    reader = open(vcf, 'r')
+    for line in reader:
+        line = line.strip()
+        if line.startswith('##'):
+            if "<ID=SVTYPE," in line:
+                continue
+            if "<ID=SVLEN," in line:
+                continue
+            if "<ID=VNTR," in line:
+                continue
+            print(line)
+            continue
+        if line.startswith('#'):
+            line = line.split('\t')
+            hap1_index = line.index(sample+'_hp1')
+            hap2_index = line.index(sample+'_hp2')
+            header = line[0:8]+[sample]
+            print('\t'.join(header))
+            continue
+        line = line.split('\t')
+        chrom = line[0]
+        start = line[1]
+        info_dict = {}
+        for field in line[7].split(';'):
+            field = field.split('=')
+            assert len(field) == 2
+            info_dict[field[0]] = field[1]
+        assert 'RU' in info_dict
+        assert 'ALTANNO' in info_dict
+        assert 'END' in info_dict
+        info_field = ';'.join([f'{s}={info_dict[s]}' for s in ['END', 'RU', 'ALTANNO']])
+        end = info_dict['END']
+        ref_seq = reference_dict[(chrom, start, end)]
+        rus = info_dict['RU'].split(',')
+        altanno = info_dict['ALTANNO'].split(',')
+        gt1 = line[hap1_index].split('/')[0]
+        gt2 = line[hap2_index].split('/')[0]
+        if gt1 == '.' and gt2 == '.':
+            continue
+        if gt1 == '.':
+            gt2 = int(gt2)
+            alt_allele_2 = convert_to_seq(altanno[gt2-1], rus)
+            if alt_allele_2 == ref_seq:
+                continue
+            else:
+                print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_2}\t.\tPASS\t{info_field}\tGT\t1/0')
+                continue
+        if gt2 == '.':
+            gt1 = int(gt1)
+            alt_allele_1 = convert_to_seq(altanno[gt1-1], rus)
+            if alt_allele_1 == ref_seq:
+                continue
+            else:
+                print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_1}\t.\tPASS\t{info_field}\tGT\t1/0')
+                continue
+        
+        alt_allele_1 = convert_to_seq(altanno[int(gt1)-1], rus)
+        alt_allele_2 = convert_to_seq(altanno[int(gt2)-1], rus)
+        if (alt_allele_1 != ref_seq) and (alt_allele_2 != ref_seq):
+            if (alt_allele_1 == alt_allele_2):
+                print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_1}\t.\tPASS\t{info_field}\tGT\t1/1')
+            else:
+                # lines have to be split
+                print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_1}\t.\tPASS\t{info_field}\tGT\t1/0')
+                print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_2}\t.\tPASS\t{info_field}\tGT\t1/0')
+            continue
+        if (alt_allele_1 == ref_seq) and (alt_allele_2 == ref_seq):
+            #skip line since both are reference
+            continue
+        if (alt_allele_1 == ref_seq):
+            print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_2}\t.\tPASS\t{info_field}\tGT\t1/0')
+        else:
+            print(f'{chrom}\t{start}\t.\t{ref_seq}\t{alt_allele_1}\t.\tPASS\t{info_field}\tGT\t1/0')
+    reader.close()
+        
+    
+if __name__=='__main__':
+    
+    parser = argparse.ArgumentParser(prog='subsample-vcf.py', description="subsample miller lab's vamos vcf and makes it compatible with the callset comparision pipeline")
+    parser.add_argument("-vcf", required=True, help="Miller Vamos VCF")
+    parser.add_argument("-sample", required=True, help="Sample to extract")
+    parser.add_argument("-reference", required=True, help="Reference BED file")
+
+    options = parser.parse_args()
+
+    run(**vars(options))
