79478c54a1b29cfd8ec34a21bbd9deceb3f55b31
Samarendra <samarendrapani1998@gmail.com>
2024-10-18 18:13:03 +0200

    output changes to make plot more readable

--- snakemake_pipelines/chimpanzee-analysis/scripts/plot-sv-lengths.py
+++ snakemake_pipelines/chimpanzee-analysis/scripts/plot-sv-lengths.py
@@ -21,18 +21,55 @@ def run(vcf=None, outdir=None):
         sv_lens.append(int(sv_id.split('-')[-1]))
     callset_reader.close()
     
+    # Plotting line plot and histogram in same plot
+    fig, ax = plt.subplots(1, 1, figsize=(10,10), tight_layout=True)
+    n, x = np.histogram(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000))
+    bin_centers = 0.5*(x[1:]+x[:-1])
+    #plt.plot(bin_centers, n, color='blue', label='All SVs')
+    ax.hist(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000), alpha=0.5 , color='orange', label='All SVs')
+    n, x = np.histogram(sv_lens, bins=range(0, max(sv_lens)+1000, 1000))
+    bin_centers = 0.5*(x[1:]+x[:-1])
+    plt.plot(bin_centers, n, color='black', label='SVs with known Ancestral All') ## using bin_centers rather than edges
+    ax.set_title('Lengths of SVs annotated with an Ancestral Allele', fontsize=16)
+    plt.xlabel('Lengths', fontsize=18)
+    plt.ylabel('Counts', fontsize=18)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(prop={'size': 16})
+    plt.yscale('log')
+    plt.savefig(outdir+'sv-length-dist.svg')
+
+    """
     # Plotting length histograms
     fig, ax = plt.subplots(1, 1, figsize=(10,10), tight_layout=True)
     ax.hist(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='blue', label='All SVs')
     ax.hist(sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='orange', label='SVs with known Ancestral All')
     ax.set_title('Lengths of SVs annotated with an Ancestral Allele', fontsize=16)
-    plt.xlabel('Lengths', fontsize=16)
-    plt.ylabel('Counts', fontsize=16)
+    plt.xlabel('Lengths', fontsize=18)
+    plt.ylabel('Counts', fontsize=18)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
-    plt.yscale('log')
     plt.legend(prop={'size': 16})
     plt.savefig(outdir+'sv-length-dist.svg')
+    
+    # Plotting length line histograms
+    fig, ax = plt.subplots(1, 1, figsize=(10,10), tight_layout=True)
+    n, x = np.histogram(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000))
+    bin_centers = 0.5*(x[1:]+x[:-1])
+    #plt.plot(bin_centers, n, color='blue', label='All SVs')
+    ax.hist(all_sv_lens, bins=range(0, max(sv_lens)+1000, 1000), color='blue', label='All SVs')
+    n, x = np.histogram(sv_lens, bins=range(0, max(sv_lens)+1000, 1000))
+    bin_centers = 0.5*(x[1:]+x[:-1])
+    plt.plot(bin_centers, n, color='orange', label='SVs with known Ancestral All') ## using bin_centers rather than edges
+    ax.set_title('Lengths of SVs annotated with an Ancestral Allele', fontsize=16)
+    plt.xlabel('Lengths', fontsize=18)
+    plt.ylabel('Counts', fontsize=18)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.legend(prop={'size': 16})
+    plt.yscale('log')
+    plt.savefig(outdir+'sv-length-dist-line.svg')
+    """
 
 if __name__=='__main__':
     
