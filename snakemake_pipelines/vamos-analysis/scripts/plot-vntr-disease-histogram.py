327ef2c5c7d0c03b841de0cb215a81ba22de65cd
Samarendra <samarendrapani1998@gmail.com>
2024-12-04 19:53:43 +0530

    updating script to plot everything with custom bins

--- snakemake_pipelines/vamos-analysis/scripts/plot-vntr-disease-histogram.py
+++ snakemake_pipelines/vamos-analysis/scripts/plot-vntr-disease-histogram.py
@@ -1,12 +1,20 @@
 import argparse
 import matplotlib.pyplot as plt
 
-def plot_histogram(h, o, output, title):
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-    plt.hist(o, 20, density=True, facecolor='blue', alpha=0.5, label='ONT')
-    plt.hist(h, 20, density=True, facecolor='green', alpha=0.5, label='HGSVC')
-    plt.legend()
-    plt.title(title)
+def plot_histogram(h, o, output, title, bins):
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    # Plotting the RUs
+    ax[0].hist(o[0], bins[0], density=True, facecolor='blue', alpha=0.5, label='ONT')
+    ax[0].hist(h[0], bins[0], density=True, facecolor='green', alpha=0.5, label='HGSVC')
+    ax[0].legend()
+    ax[0].set_title(title+' (in RUs)')
+    
+    # Plotting the base pairs
+    ax[1].hist(o[1], bins[1], density=True, facecolor='blue', alpha=0.5, label='ONT')
+    ax[1].hist(h[1], bins[1], density=True, facecolor='green', alpha=0.5, label='HGSVC')
+    ax[1].legend()
+    ax[1].set_title(title+' (in base pairs)')
+
     plt.savefig(output)
     pass
 
@@ -40,14 +48,14 @@ def run(hgsvc, ont, output):
     ont_num_rus, ont_bps = get_statistics(ont)
 
     # plotting the ABCA7 basepairs
-    abca7_h = hgsvc_bps['ABCA7']
-    abca7_o = ont_bps['ABCA7']
-    plot_histogram(h=abca7_h, o=abca7_o, output=output+'abca7-histogram.svg', title='ABCA7 (in bps)')
+    bins_bps = [500*i for i in range(21)]
+    bins_num_rus = [i for i in range(0, 401, 20)]
+    plot_histogram(h=(hgsvc_num_rus['ABCA7'], hgsvc_bps['ABCA7']), o=(ont_num_rus['ABCA7'], ont_bps['ABCA7']), output=output+'abca7-histogram.svg', title='ABCA7', bins=(bins_num_rus, bins_bps))
 
     # plotting the PLIN4 RUs
-    plin4_h = hgsvc_num_rus['PLIN4']
-    plin4_o = ont_num_rus['PLIN4']
-    plot_histogram(h=plin4_h, o=plin4_o, output=output+'plin4-histogram.svg', title='PLIN4 (in RUs)')
+    bins_num_rus = [i for i in range(10, 45, 2)]
+    bins_bps = [i for i in range(1000, 4401, 200)]
+    plot_histogram(h=(hgsvc_num_rus['PLIN4'], hgsvc_bps['PLIN4']), o=(ont_num_rus['PLIN4'], ont_bps['PLIN4']), output=output+'plin4-histogram.svg', title='PLIN4', bins=(bins_num_rus, bins_bps))
 
 
 if __name__=='__main__':
